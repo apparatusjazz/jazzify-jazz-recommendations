5f94021442b822b4781fa69a8bef74b00ef2aed5
apparatusjazz <maui.arcuri@gmail.com>
2020-10-17 23:03:13 -0400

    Added base jazz collection

---
+++ src/jazz-collection.js
@@ -0,0 +1,316 @@
+const jazzCollection = {
+
+    "acoustic": [
+        "2TSuAchdgVzsAa9wDK1IeT"
+    ],
+    "afro": [
+        "5LHgdJhFO2hT2gu877UPKV",
+        "1gMQhZV6otY4KEVN3yujDJ"
+    ],
+    "ambient": [
+        "1FC0psUheo5L2kUtj53MF9",
+        "0azLE0hIUlbMbV0g2Ezt9m",
+        "0Z5FMozvx15nUSUA6a9kkU",
+        "7sYipTRgDXS2JVOPEhRutx"
+    ],
+    "anime": [
+        "3uoY3Ibj2qOK3bb47cpKs6",
+        "6cdmYp8fPQYDIU1l1Sqt8X"
+    ],
+    "bigband": [
+        "1W8TbFzNS15VwsempfY12H",
+        "6ra4GIOgCZQZMOaUECftGN",
+        "6HQYnRM4OzToCYPpVBInuU",
+        "2PjgZkwAEk7UTin4jP6HLP",
+        "7ENzCHnmJUr20nUjoZ0zZ1",
+        "0EodhzA6yW1bIdD5B4tcmJ",
+        "1pBuKaLHJlIlqYxQQaflve",
+        "1GxkXlMwML1oSg5eLPiAz3",
+        "5RdEgvFMoLubrYet7DNjia"
+    ],
+    "bluegrass": [
+        "5Zmur9D9gpr6tXyDrpnbOe"
+    ],
+    "blues": [
+        "2TSuAchdgVzsAa9wDK1IeT",
+        "1oTIG44ppYw0nUBqFvZ5Q5",
+        "0uyK6faAhm4CYcf1OhP38V"
+    ],
+    "bop": [
+        "68u79CqTqTgKncfDRbjDLH",
+        "6ykfXAed2KOLOMI3R0TZdz",
+        "1W8TbFzNS15VwsempfY12H",
+        "2hGh5VOeeqimQFxqXvfCUf"
+    ],
+    "bossanova": [
+        "3pO5VjZ4wOHCMBXOvbMISG",
+        "77ZUbcdoU5KCPHNUl8bgQy",
+        "0yFvXd36g5sNKYDi0Kkvl8"
+    ],
+    "chill": [
+        "497rp5TEzJffeBnUT0BeE1",
+        "01mXk9IDlVczWwZvVHAiIS",
+        "3Rq3YOF9YG9YfCWD4D56RZ",
+        "6mWAKV1AAFvzxQr7uztRE9"
+    ],
+    "classic": [
+        "0BqALs1lInR9TTOulUADH7",
+        "3pO5VjZ4wOHCMBXOvbMISG",
+        "6ykfXAed2KOLOMI3R0TZdz",
+        "6pVkGhtBvjj1P52yn0tNLG",
+        "4jXfFzeP66Zy67HM2mvIIF",
+        "1W8TbFzNS15VwsempfY12H",
+        "2hGh5VOeeqimQFxqXvfCUf"
+    ],
+    "classical": [
+        "2TSuAchdgVzsAa9wDK1IeT",
+        "68GFLV3RsMCGeWc3JFbijD",
+        "79vrVvasPxDip9pGVi4p8B"
+    ],
+    "comedy": [
+        "6ra4GIOgCZQZMOaUECftGN"
+    ],
+    "country": [
+        "5Zmur9D9gpr6tXyDrpnbOe",
+        "6BXA5FDonefcAe2hg4T7K5",
+        "4dZrt8Ong5t7YYpvbfp0RU"
+    ],
+    "dance": [
+        "2ev6Cd0yJVCcpf2zezEQ8Z",
+        "3TMLgJUsDIxI4CShGh8vYM"
+    ],
+    "drum and bass": [
+        "19f2JXwlRU26376TCKmp6L",
+        "497rp5TEzJffeBnUT0BeE1",
+        "0azLE0hIUlbMbV0g2Ezt9m"
+    ],
+    "electronic": [
+        "2ev6Cd0yJVCcpf2zezEQ8Z",
+        "19f2JXwlRU26376TCKmp6L",
+        "0azLE0hIUlbMbV0g2Ezt9m",
+        "3TMLgJUsDIxI4CShGh8vYM",
+        "5mUcc8OOP4RuzrupeGYwW5",
+        "0Z5FMozvx15nUSUA6a9kkU",
+        "6mWAKV1AAFvzxQr7uztRE9"
+    ],
+    "experimental": [
+        "29XOeO6KIWxGthejQqn793",
+        "0Z5FMozvx15nUSUA6a9kkU",
+        "0tIODqvzGUoEaK26rK4pvX",
+        "3poNEhEAAwqWGnLaQNE0Fs"
+    ],
+    "folk": [
+        "2TSuAchdgVzsAa9wDK1IeT",
+        "5Zmur9D9gpr6tXyDrpnbOe",
+        "6BXA5FDonefcAe2hg4T7K5",
+        "0D3h8NZqNp7BN97JwtV6eW"
+    ],
+    "funk": [
+        "44PWvdyxyL5P8eXZTP2HDI",
+        "2i1CPudyCUjL50Wqjv8AMI",
+        "65dGLGjkw3UbddUg2GKQoZ",
+        "4YLUMAgNyttwx4hUHgtBtR",
+        "7ENzCHnmJUr20nUjoZ0zZ1",
+        "0uyK6faAhm4CYcf1OhP38V",
+        "5COMNbVWoe1Kb5UTFAaUwF"
+    ],
+    "fusion": [
+        "5CK3a77IzCSHjNqzRcbhuG",
+        "1V4jsLjkic1Mxvdke86Qth",
+        "5Zmur9D9gpr6tXyDrpnbOe",
+        "6ra4GIOgCZQZMOaUECftGN"
+    ],
+    "guitar": [
+        "68u79CqTqTgKncfDRbjDLH",
+        "2TSuAchdgVzsAa9wDK1IeT",
+        "5CK3a77IzCSHjNqzRcbhuG",
+        "1V4jsLjkic1Mxvdke86Qth",
+        "6pVkGhtBvjj1P52yn0tNLG",
+        "2CH9bD5RtnDcm9O23JByN7",
+        "1oTIG44ppYw0nUBqFvZ5Q5",
+        "14RXohtx6NiBGFTW8IdmAK",
+        "15kazQHHvIeFD1WYYjlFXG"
+    ],
+    "happy": [
+        "6RuS4udHwx3C9ysk4KXtNw"
+    ],
+    "heavy": [
+        "0tIODqvzGUoEaK26rK4pvX",
+        "3poNEhEAAwqWGnLaQNE0Fs",
+        "6NO4yN3PJL0PeMBPgdBWGq"
+    ],
+    "hiphop": [
+        "2i1CPudyCUjL50Wqjv8AMI",
+        "4ai53dgSBGhQwcFtGyY1bF",
+        "4YLUMAgNyttwx4hUHgtBtR",
+        "3TMLgJUsDIxI4CShGh8vYM",
+        "2q37Nw8NND2z1T1KU5XVfn",
+        "5mUcc8OOP4RuzrupeGYwW5",
+        "29XOeO6KIWxGthejQqn793",
+        "710FHFQeXKC6dZGCxt7kQh",
+        "01mXk9IDlVczWwZvVHAiIS",
+        "3Rq3YOF9YG9YfCWD4D56RZ"
+    ],
+    "indie": [],
+    "japanese": [
+        "3gi1r8g39AZGZO7iwEw4lh",
+        "6RuS4udHwx3C9ysk4KXtNw",
+        "7sEmXHrnEnX7PScoJAvSvo",
+        "7MewDkQnL8tMeWEBxSWBE2",
+        "6RuS4udHwx3C9ysk4KXtNw",
+        "52uuAX0XAdhMmLLpaSaOig",
+        "5vLyvkGLk9d9hp9LRQ8RqD",
+        "5COMNbVWoe1Kb5UTFAaUwF",
+        "0rpKM0MniNkXM1SLSglYUZ",
+        "5RdEgvFMoLubrYet7DNjia"
+    ],
+    "jazz": [
+        "68u79CqTqTgKncfDRbjDLH",
+        "6ykfXAed2KOLOMI3R0TZdz",
+        "1W8TbFzNS15VwsempfY12H",
+        "2hGh5VOeeqimQFxqXvfCUf"
+    ],
+    "kids": [],
+    "latin": [
+        "1dp5YyefJVa9mkiqa3s50O",
+        "3pO5VjZ4wOHCMBXOvbMISG",
+        "68GFLV3RsMCGeWc3JFbijD",
+        "5LHgdJhFO2hT2gu877UPKV",
+        "0MGQZNEMaEICyku3cSDrnJ",
+        "77ZUbcdoU5KCPHNUl8bgQy",
+        "0yFvXd36g5sNKYDi0Kkvl8",
+        "6sMYDCMbXwodgyR2vejSEb",
+        "15kazQHHvIeFD1WYYjlFXG",
+        "1gMQhZV6otY4KEVN3yujDJ"
+
+    ],
+    "metal": [
+        "3poNEhEAAwqWGnLaQNE0Fs",
+        "6NO4yN3PJL0PeMBPgdBWGq",
+        "0D3h8NZqNp7BN97JwtV6eW",
+        "7BDSgVNphGXwxPxyC0lR3P"
+    ],
+    "modern": [
+        "4ai53dgSBGhQwcFtGyY1bF",
+        "68GFLV3RsMCGeWc3JFbijD",
+        "1P6Llrp12ldpVbyC8gCHBz",
+        "79vrVvasPxDip9pGVi4p8B",
+        "5qOfTfMzTj2dvvpWKzIFk6",
+        "1FC0psUheo5L2kUtj53MF9",
+        "2CH9bD5RtnDcm9O23JByN7",
+        "0gaASCUAMqvXxGFDYaNFca",
+        "2vI9KFm0fwSfPrpEgOeIbq",
+        "6HQYnRM4OzToCYPpVBInuU",
+        "2q37Nw8NND2z1T1KU5XVfn",
+        "6BXA5FDonefcAe2hg4T7K5"
+    ],
+    "old": [
+        "6htazaFMy8zs0f3sMtM2Pt",
+        "2PjgZkwAEk7UTin4jP6HLP",
+        "0EodhzA6yW1bIdD5B4tcmJ",
+        "1pBuKaLHJlIlqYxQQaflve"
+    ],
+    "piano": [
+        "22KzEvCtrTGf9l6k7zFcdv",
+        "0BqALs1lInR9TTOulUADH7",
+        "44PWvdyxyL5P8eXZTP2HDI",
+        "3gi1r8g39AZGZO7iwEw4lh",
+        "2ev6Cd0yJVCcpf2zezEQ8Z",
+        "4jXfFzeP66Zy67HM2mvIIF",
+        "2vI9KFm0fwSfPrpEgOeIbq",
+        "0FcNSKwWZJb98ry9M2qEII",
+        "0D3h8NZqNp7BN97JwtV6eW",
+        "6mWAKV1AAFvzxQr7uztRE9",
+        "7sEmXHrnEnX7PScoJAvSvo"
+    ],
+    "pop": [
+        "1GxkXlMwML1oSg5eLPiAz3",
+        "7w5X03XwXcR7pxV6iY1swZ",
+        "2Kx7MNY7cI1ENniW7vT30N",
+        "6MVeEwYSJUvwwYfUVPeZ6Y",
+        "0JvUFDnuWPbuyRa6ptVkLk",
+        "6niRXUE5IPchgYvYD8Ck00",
+        "2VU83NBXbWqEuBZVI52YtL"
+    ],
+    "post bop": [
+        "1P6Llrp12ldpVbyC8gCHBz",
+        "2vI9KFm0fwSfPrpEgOeIbq"
+    ],
+    "reggae": [
+        "30V1rKijENF5MFcGidInfh",
+        "5L59UhTBOeIRPxF5umwC9p",
+        "6RTCb5pWKinqlUyG2qacit",
+        "5LpfSc34hwEqnPO4ayZFev",
+        "1Jx0wDdUle4mnEaOAGdC2H",
+        "0ADVX4vKQN1saCsOdl67Fj",
+        "4B6xk9HVjlWYmJRJoBhTAx",
+        "7BNUk7Gaw629fGVmoMCNsE"
+    ],
+    "rap": [
+        "4YLUMAgNyttwx4hUHgtBtR",
+        "29XOeO6KIWxGthejQqn793",
+        "3Rq3YOF9YG9YfCWD4D56RZ",
+        "5BRAUN0yN8557PLRZIr02W",
+        "5HNkGissAKlCv88sus7rVO",
+        "3CJEpzlVzfyLTpKJlpKdHw"
+    ],
+    "rnb": [
+        "60k4atxgEIMFz4DWHp0le1",
+        "5bepW5vcdRzheNc0F8lHJ5"
+    ],
+    "rock": [
+        "5CK3a77IzCSHjNqzRcbhuG",
+        "1V4jsLjkic1Mxvdke86Qth",
+        "6ra4GIOgCZQZMOaUECftGN",
+        "7ENzCHnmJUr20nUjoZ0zZ1",
+        "710FHFQeXKC6dZGCxt7kQh",
+        "1OCdpTg8d7Ehpj8B9j4iqa",
+        "14RXohtx6NiBGFTW8IdmAK",
+        "6RuS4udHwx3C9ysk4KXtNw",
+        "0rpKM0MniNkXM1SLSglYUZ",
+        "5A0MH7JfEBEMySevsmauds",
+        "3nYGhK3zYoTm2Yz1KduuUM"
+    ],
+    "sad": [],
+    "saxophone": [
+        "5qOfTfMzTj2dvvpWKzIFk6",
+        "0gaASCUAMqvXxGFDYaNFca",
+        "60k4atxgEIMFz4DWHp0le1",
+        "0tIODqvzGUoEaK26rK4pvX",
+        "2hGh5VOeeqimQFxqXvfCUf"
+    ],
+    "ska": [],
+    "smooth": [],
+    "soul": [
+        "65dGLGjkw3UbddUg2GKQoZ",
+        "60k4atxgEIMFz4DWHp0le1",
+        "6HQYnRM4OzToCYPpVBInuU",
+        "0FcNSKwWZJb98ry9M2qEII",
+        "710FHFQeXKC6dZGCxt7kQh",
+        "0uyK6faAhm4CYcf1OhP38V"
+    ],
+    "spanish": [
+        "1dp5YyefJVa9mkiqa3s50O"
+    ],
+    "swing": [
+        "2PjgZkwAEk7UTin4jP6HLP"
+    ],
+    "trap": [
+        "2q37Nw8NND2z1T1KU5XVfn"
+    ],
+    "trumpet": [
+        "4ai53dgSBGhQwcFtGyY1bF",
+        "2q37Nw8NND2z1T1KU5XVfn"
+    ],
+    "vocal": [
+        "3gi1r8g39AZGZO7iwEw4lh",
+        "3pO5VjZ4wOHCMBXOvbMISG",
+        "6htazaFMy8zs0f3sMtM2Pt",
+        "60k4atxgEIMFz4DWHp0le1",
+        "0EodhzA6yW1bIdD5B4tcmJ",
+        "1GxkXlMwML1oSg5eLPiAz3"
+    ],
+    "world": []
+}
+
+export default jazzCollection;
