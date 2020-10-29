8fbf53ff169cb3f8bc951c6ced5ffb8de532ec6c
apparatusjazz <maui.arcuri@gmail.com>
2020-10-29 16:52:57 -0400

    Adjusted min and max values and tempo

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import { Slider } from '@material-ui/core';
+import '../css/filters.css';
 
 class Filter extends Component {
     handleChange(val) {
-        this.props.storeValue(this.props.id, val)
+        let value = this.props.name === "target_tempo" ? val : val / 100;
+        this.props.storeValue(this.props.name, value, this.props.type)
     }
     render() {
         return (
             <>
+                <div className="filter-name">{this.props.name}</div>
                 <Slider
+                    key={this.props.name}
                     value={this.props.value}
                     aria-labelledby="continuous-slider"
-                    min={0}
-                    max={1}
+                    min={this.props.min}
+                    max={this.props.max}
                     onChange={(event, val) => this.handleChange(val)}
                 />
             </>
