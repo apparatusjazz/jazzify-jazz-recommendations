df19e4ffde4a9c6927eb6ea88adb2a0c8b43ae42
apparatusjazz <maui.arcuri@gmail.com>
2020-11-12 01:35:41 -0500

    Added remove genre button

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import { Slider } from '@material-ui/core';
 import '../css/filters.css';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
 class Filter extends Component {
     handleChange(val) {
         let value = this.props.name === "target_tempo" ? val : val / 100;
         this.props.storeValue(this.props.name, value, this.props.type)
     }
+    handleClick() {
+        this.props.removeGenre(this.props.name);
+    }
     render() {
         return (
             <>
                 <div className="filter-name">{this.props.name}</div>
+                <HighlightOffIcon className="remove-genre" onClick={() => { this.handleClick() }} />
                 <Slider
                     key={this.props.name}
                     value={this.props.value}
