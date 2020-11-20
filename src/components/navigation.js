b6911f56c7585eac47b6c8c55e92b998d970d73f
apparatusjazz <maui.arcuri@gmail.com>
2020-11-20 17:40:12 -0500

    Added logout button

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,14 +3,17 @@ import { Navbar, Nav } from 'react-bootstrap';
 import '../css/navigation.css';
 
 class Navigation extends Component {
-
+    logout() {
+        window.location.hash = '';
+        window.location.reload();
+    }
     render() {
         return (
             <>
                 <Navbar className="main-nav" bg="dark" variant="dark" expand="md">
                     <Navbar.Brand className="branding" >Jazzify</Navbar.Brand>
                     <Nav className="ml-auto nav-right">
-                        <a target="_blank" rel="noopener noreferrer" className="btn" id="logout-btn" href={"https://www.spotify.com/logout"}>
+                        <a target="_blank" rel="noopener noreferrer" className="btn" onClick={() => this.logout()} id="logout-btn" href={"https://www.spotify.com/logout"}>
                             Logout
                         </a>
                     </Nav>
