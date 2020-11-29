130084e548e920b2466fc8c6962caf34485c2c65
apparatusjazz <maui.arcuri@gmail.com>
2020-11-28 23:55:06 -0500

    Added default behavior

--- src/components/player.js
+++ src/components/player.js
@@ -2,16 +2,18 @@ import React from 'react';
 import '../css/player.css';
 import PlayArrowRoundedIcon from '@material-ui/icons/PlayArrowRounded';
 import PauseCircleFilledRoundedIcon from '@material-ui/icons/PauseCircleFilledRounded';
+import AlbumIcon from '@material-ui/icons/Album';
 
 
 const Player = (props) => {
-
+    console.log(props)
+    const defaultAlbum = <AlbumIcon className="p-el default-album" />
     const play = !props.playing ? <PlayArrowRoundedIcon className="play" /> : <PauseCircleFilledRoundedIcon className="pause" />;
     return (
         <>
             <div className="player">
                 <div className="p-container">
-                    <img className="p-el album-player" src={props.img} />
+                    {props.img === '' ? defaultAlbum : <img className="p-el album-player" src={props.img} />}
                     <div className="p-el info-container">
                         <div className="p-el song">{props.songName}</div>
                         <div className="p-el artist">{props.artistName}</div>
