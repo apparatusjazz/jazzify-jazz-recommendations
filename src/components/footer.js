c231b22758dd319792ec244a3db1d4f1071ce756
apparatusjazz <maui.arcuri@gmail.com>
2020-12-06 12:27:33 -0500

    Adjusted footer styl and links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,30 @@
 import About from './about';
 import React, { useState } from 'react';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import '../css/footer.css';
 
 const Footer = (props) => {
     const [modalShow, setModalShow] = useState(false);
 
     return (
-        <>
-            Project created by Maui Arcuri
-            <GitHubIcon />
-            <a onClick={() => setModalShow(true)}>
+        <div className="footer">
+            Project created by Maui Arcuri:
+            <a
+                href="https://github.com/apparatusjazz/jazzify-jazz-recommendations"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Open in Spotify"
+            >
+                <GitHubIcon className="github" />
+            </a>
+            <button className="about-tag" onClick={() => setModalShow(true)}>
                 About Jazzify
-                        </a>
+                        </button>
             <About
                 show={modalShow}
                 onHide={() => setModalShow(false)}
             />
-        </>
+        </div>
     )
 }
 
